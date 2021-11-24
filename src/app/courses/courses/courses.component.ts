1291c726bfff8c93fe4b9cd293ae16b9d944cefb
Educord97 <eduuhliima97@gmail.com>
2021-11-24 10:07:53 -0300

    icons an list

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+
+import { catchError } from 'rxjs/operators'
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -10,11 +14,26 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable <Course[]>;
+  courses$: Observable <Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private courseService: CoursesService) {
-    this.courses = this.courseService.list();
+  constructor(
+    private courseService: CoursesService,
+    private dialog: MatDialog
+    ) {
+    this.courses$ = this.courseService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos.')
+        return of([])
+      })
+    );
+   }
+
+   onError(errorMsg: string) {
+     this.dialog.open(ErrorDialogComponent, {
+       data: errorMsg
+     });
    }
 
   ngOnInit(): void {
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
-import { first, tap } from 'rxjs/operators';
+import { delay, first, tap } from 'rxjs/operators';
 
 import { HttpClient } from '@angular/common/http';
 
@@ -17,6 +17,7 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
+     // delay(5000),
       tap(courses => console.log(courses))
     )
   }
